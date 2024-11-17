ce64195da5faf6515ce4be4298e54d299460a4ac
Abu Bokkor Ratul <159571533+Abubokkorratul@users.noreply.github.com>
2024-11-18 01:04:29 +0600

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-
+n = int(input("Enter a number: "))
+a, b = 0, 1
+is_fibonacci = False
+while a <= n:
+    if a == n:
+        is_fibonacci = True
+        break
+    a, b = b, a + b
+if is_fibonacci:
+    print(f"{n} is a Fibonacci number")
+else:
+    print(f"{n} is not a Fibonacci number")
